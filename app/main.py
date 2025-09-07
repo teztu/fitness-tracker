9e57ac81b30e7ff6ed713894a6903e4cd0c58d72
teztu <nicowc@gmail.com>
2025-09-07 17:14:08 +0200

    chore: stop tracking .env; ignore nested __pycache__; repo cleanup

--- app/main.py
+++ app/main.py
@@ -1,8 +1,38 @@
-﻿from fastapi import FastAPI
+﻿from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+#from .schemas import WeightInput
+from datetime import datetime
 
 app = FastAPI()
 
 @app.get("/health")
 def health():
-    return {"status": "everything stable"}
+    return {"status": "everything stable", "timestamp": datetime.now()}
 
+
+@app.post("/weigh_in")
+def entry_weight():
+    try:
+        """Enter todays weight"""
+        return weight_data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"generic error message: {str(e)}")
+
+
+@app.get("/weight/latest")
+def get_latest_weight():
+    try:
+
+        """Get the most recent weight entry"""
+        return latest_weight
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"generic error message: {str(e)}")
+
+@app.delete("/weight/{weight_id}")
+def delete_weight():
+    try:
+
+        """Delete a weight entry"""
+        return deleted_weight
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"generic error message: {str(e)}")
