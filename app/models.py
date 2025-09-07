031da16203c02a46b3f633067c5b6311c66d4921
teztu <nicowc@gmail.com>
2025-09-07 21:30:20 +0200

    Added table weight

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
-﻿
+﻿from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date, Float
+from .database import Base
+
+class Weight(Base):
+    __tablename__ = "bodyweight"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(Date, nullable=False)
+    kg = Column(Float, nullable=False) #not realistic weighing 0 kg
+
 
 
