1c5c8df8bd57bc43cd656a5fa8d687952430ca45
teztu <nicowc@gmail.com>
2025-09-07 21:50:26 +0200

    added pydantic classes

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,25 @@
-﻿#class InputWeight(BaseModel):
-    #weight_input: float
+﻿from datetime import date
+from pydantic import BaseModel, Field, ConfigDict
+
+class EnterWeight(BaseModel):
+    """expected of user to type"""
+    date: date
+    kg: float = Field(gt=0, description="BW in kilograms")
+
+
+class WeightOut(BaseModel):
+    """when requesting weight"""
+    id: int
+    date: date
+    kg: float
+
+    # access object directly
+    model_config = ConfigDict(from_attributes=True)
+
+
+class WeightLatestOut(WeightOut):
+
+    pass
 
 
 
